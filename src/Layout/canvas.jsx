fc2ed0e3fad0c10ddda9a6572bc6673131396506
Marcos Fernández <marcos.fersu@gmail.com>
2023-01-05 17:21:49 -0300

    change rute

--- src/Layout/canvas.jsx
+++ src/Layout/canvas.jsx
@@ -139,7 +139,7 @@ class Canvas extends React.Component {
     this.videoTexture = {};
 
     this.video["screen"] = document.createElement("video");
-    this.video["screen"].src = "./public/pokemon-leafgreen.mp4";
+    this.video["screen"].src = "pokemon-leafgreen.mp4";
     this.video["screen"].muted = true;
     this.video["screen"].playsInline = true;
     //this.video["screen"].autoplay = true;
